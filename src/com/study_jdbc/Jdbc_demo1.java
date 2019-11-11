8592fc86bf3d9c7402193641854a7ac40617fc16
Vulner-6 <1141613511@qq.com>
2019-11-11 23:15:27 +0800

    2019.11.11练习jdbc快速入门

---
+++ src/com/study_jdbc/Jdbc_demo1.java
@@ -0,0 +1,35 @@
+package com.study_jdbc;
+import java.sql.*;
+import com.mysql.jdbc.Driver;
+import java.lang.*;
+import java.util.concurrent.locks.Condition;
+
+/**
+ * 练习连接本地mysql数据库，进行更新数据表操作
+ * 1.导入驱动jar包（已经下载、导入了）
+ * 2.注册mysql驱动
+ * 3.获取数据库连接对象
+ * 4.定义sql查询语句
+ * 5.获取执行sql对象
+ */
+public class Jdbc_demo1 {
+    public static void main(String[] args) throws Exception{
+        //1.导入驱动jar包（已经下载、导入了）
+        //2.注册mysql驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //3.获取数据库连接对象
+        Connection localhost=DriverManager.getConnection("jdbc:mysql://localhost:3306/study_jdbc?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone = GMT","root","root");
+        //4.定义sql语句
+        String sql="UPDATE users SET age=33 WHERE id = 1";
+        //5.获取sql对象Statement
+        Statement stmt=localhost.createStatement();
+        //6.执行sql
+        int count=stmt.executeUpdate(sql);
+        //7.查看处理结果
+        System.out.println(count);
+        //8.释放资源
+        stmt.close();
+        localhost.close();
+    }
+
+}
